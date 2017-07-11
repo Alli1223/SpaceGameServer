53fee751b6ed3e7e3d62a29952a22ffd3650c38c
Alastair Rayner <alli0@me.com>
2017-07-11 20:37:36 +0100

    Changed the map from an array of arrays to key value pairs

--- Map.java
+++ Map.java
@@ -2,16 +2,102 @@
 
 import javafx.scene.control.*;
 
+import java.util.Collection;
 import java.util.NavigableMap;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class Map {
 
 	private static Map map = new Map();
 	private int cellSize = 50;
+	private int EditableMapSize = 1000;
 
-    ConcurrentHashMap<Integer,ConcurrentHashMap<Integer, Chunk>> World = new ConcurrentHashMap<Integer,ConcurrentHashMap<Integer, Chunk>>(1000);
+	// Old cell system
+    //private Cell[][] cells = new Cell[3000][3000];
 
+    ConcurrentMap<Pair, Cell> tiles = new ConcurrentMap<Pair, Cell>() {
+        @Override
+        public Cell putIfAbsent(Pair key, Cell value) {
+            return null;
+        }
+
+        @Override
+        public boolean remove(Object key, Object value) {
+            return false;
+        }
+
+        @Override
+        public boolean replace(Pair key, Cell oldValue, Cell newValue) {
+            return false;
+        }
+
+        @Override
+        public Cell replace(Pair key, Cell value) {
+            return null;
+        }
+
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean containsKey(Object key) {
+            return false;
+        }
+
+        @Override
+        public boolean containsValue(Object value) {
+            return false;
+        }
+
+        @Override
+        public Cell get(Object key) {
+            return null;
+        }
+
+        @Override
+        public Cell put(Pair key, Cell value) {
+            return null;
+        }
+
+        @Override
+        public Cell remove(Object key) {
+            return null;
+        }
+
+        @Override
+        public void putAll(java.util.Map<? extends Pair, ? extends Cell> m) {
+
+        }
+
+        @Override
+        public void clear() {
+
+        }
+
+        @Override
+        public Set<Pair> keySet() {
+            return null;
+        }
+
+        @Override
+        public Collection<Cell> values() {
+            return null;
+        }
+
+        @Override
+        public Set<Entry<Pair, Cell>> entrySet() {
+            return null;
+        }
+    };
 
 
 	public static Map getInstance() {
@@ -28,24 +114,21 @@ public class Map {
 
 		// populates the map
 		System.out.println("Starting Map Gen");
-		for (int x = 0; x < cells.length; x++) {
-			System.out.println("Generating Map: " + x + " OF " + cells.length);
-			for (int y = 0; y < cells[0].length; y++) {
-				cells[x][y] = new Cell();
+		for (int x = -EditableMapSize; x < EditableMapSize; x++) {
+			System.out.println("Generating Map: " + x + " OF " + EditableMapSize);
+			for (int y = -EditableMapSize; y < EditableMapSize; y++) {
+                tiles.put(new Pair(x, y), new Cell());
 			}
 		}
 		System.out.println("Map Complete");
 	}
 
-	private Cell[][] cells = new Cell[3000][3000];
+	//private Cell[][] cells = new Cell[3000][3000];
 
 	protected void setCell(int x, int y, String content) {
-		getCells()[x][y].setCellContent(content);
+        tiles.get(new Pair(x, y)).setCellContent(content);
 	}
 
-	protected Cell[][] getCells() {
-		return cells;
-	}
 
 	// uncomment if we go back into a tree based system:
 
