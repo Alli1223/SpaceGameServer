12fc455e8343e15014d38b2d1f7903ecb665362e
Alastair Rayner <alli0@me.com>
2017-07-28 16:11:53 +0100

    Clean up of code and start of json network refactor

--- TCPServer.java
+++ TCPServer.java
@@ -3,57 +3,56 @@ import java.io.PrintStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.ServerSocket;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 //Source From http://makemobiapps.blogspot.co.uk/p/multiple-client-server-chat-programming.html
 
-/*
- * A chat server that delivers public and private messages.
- */
 public class TCPServer {
-	StringBuilder AllActions = new StringBuilder();
-	private static TCPServer tCPServer = new TCPServer();
-	
-	public String outputCommands = null;
+    StringBuilder AllActions = new StringBuilder();
+    private static TCPServer tCPServer = new TCPServer();
 
-	// The server socket.
-	private static ServerSocket serverSocket = null;
-	// The client socket.
-	private static Socket clientSocket = null;
-	
-	public int numOfConnectedClients = 0;
+    public String outputCommands = null;
 
-	// This chat server can accept up to maxClientsCount clients' connections.
-	private static final int maxClientsCount = 40;
-	private static final clientThread[] threads = new clientThread[maxClientsCount];
+    // The server socket.
+    private static ServerSocket serverSocket = null;
+    // The client socket.
+    private static Socket clientSocket = null;
 
-	//Our Code////////////////////////////////////////////////////////////
-	public clientThread[] GetThreads() {
-		return threads;
-	}
-	
-	//Get the instance of this server
-	public static TCPServer getInstance(){
-		return tCPServer;
-	}
-	
-	private TCPServer(){}
-	
-	//Our code ends////////////////////////////////////////////////////////
-	public static void main(String args[]) throws JSONException {
+    public int numOfConnectedClients = 0;
 
-		// The default port number.
-		int portNumber = 2222;
-		if (args.length < 1) {
-			System.out.println(
-					"Server running on port number: " + portNumber);
-		}
-		//Run on other specified port
-		else {
-			portNumber = Integer.valueOf(args[0]).intValue();
-		}
-		
+    // This chat server can accept up to maxClientsCount clients' connections.
+    private static final int maxClientsCount = 40;
+    private static final clientThread[] threads = new clientThread[maxClientsCount];
+
+    //Our Code////////////////////////////////////////////////////////////
+    public clientThread[] GetThreads() {
+        return threads;
+    }
+
+    //Get the instance of this server
+    public static TCPServer getInstance() {
+        return tCPServer;
+    }
+
+    private TCPServer() {
+    }
+
+    //Our code ends////////////////////////////////////////////////////////
+    public static void main(String args[]) throws JSONException {
+
+        // The default port number.
+        int portNumber = 2222;
+        if (args.length < 1) {
+            System.out.println(
+                    "Server running on port number: " + portNumber);
+        }
+        //Run on other specified port
+        else {
+            portNumber = Integer.valueOf(args[0]).intValue();
+        }
+
 		
 
 		/*
@@ -61,235 +60,210 @@ public class TCPServer {
 		 * can not choose a port less than 1023 if we are not privileged users
 		 * (root).
 		 */
-		try {
-			serverSocket = new ServerSocket(portNumber);
-		} catch (IOException e) {
-			System.out.println(e);
-		}
+        try {
+            serverSocket = new ServerSocket(portNumber);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
 
 		/*
 		 * Create a client socket for each connection and pass it to a new
 		 * client thread.
 		 */
-		while (true) {
-			try {
-				clientSocket = serverSocket.accept();
-				int i = 0;
-				for (i = 0; i < maxClientsCount; i++) 
-				{
-					if (threads[i] == null) 
-					{
-						// Create new client thread
-						(threads[i] = new clientThread(clientSocket, threads, i)).start();
-						break;
-					}
-				}
-				if (i == maxClientsCount) {
-					PrintStream os = new PrintStream(clientSocket.getOutputStream());
-					os.println("Server too busy. Try later.");
-					os.close();
-					clientSocket.close();
-				}
-			} catch (IOException e) {
-				System.out.println(e);
-			}
-		}
-	}
+        while (true) {
+            try {
+                clientSocket = serverSocket.accept();
+                int i = 0;
+                for (i = 0; i < maxClientsCount; i++) {
+                    if (threads[i] == null) {
+                        // Create new client thread
+                        (threads[i] = new clientThread(clientSocket, threads, i)).start();
+                        break;
+                    }
+                }
+                if (i == maxClientsCount) {
+                    PrintStream os = new PrintStream(clientSocket.getOutputStream());
+                    os.println("Server too busy. Try later.");
+                    os.close();
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+    }
 }
 
 // Client thread //////////////////////////////
 class clientThread extends Thread {
 
-	private String clientName = null;
-	private DataInputStream inStream = null;
-	private PrintStream outStream = null;
-	private Socket clientSocket = null;
-	private final clientThread[] threads;
-	//Our Code////////////////////////////////////////////////////////////
+    private String clientName = null;
+    private DataInputStream inStream = null;
+    private PrintStream outStream = null;
+    private Socket clientSocket = null;
+    private final clientThread[] threads;
+    //Our Code////////////////////////////////////////////////////////////
 
-	private InputReader ir = new InputReader();
-	private int maxClientsCount;
-	private Character character = new Character(100, 100);
-	private World map = World.getInstance();
-	private final int ID;
-	
+    private InputReader ir = new InputReader();
+    private int maxClientsCount;
+    private Character character = new Character(100, 100);
+    private World map = World.getInstance();
+    private final int ID;
 
-	public clientThread(Socket clientSocket, clientThread[] threads, int ID) {
-		this.clientSocket = clientSocket;
-		this.threads = threads;
-		this.ID = ID;
-		maxClientsCount = threads.length;
-	}
 
-	public int getID(){
-		return ID;
-	}
-	
+    public clientThread(Socket clientSocket, clientThread[] threads, int ID) {
+        this.clientSocket = clientSocket;
+        this.threads = threads;
+        this.ID = ID;
+        maxClientsCount = threads.length;
+    }
 
-	@SuppressWarnings("deprecation")
-	public void run()  {
-		int maxClientsCount = this.maxClientsCount;
+    public int getID() {
+        return ID;
+    }
 
-		clientThread[] threads = this.threads;
-		
 
-		// Create streams and ask their name
-		try {
+    @SuppressWarnings("deprecation")
+    public void run() {
+        int maxClientsCount = this.maxClientsCount;
+
+        clientThread[] threads = this.threads;
+
+
+        // Create streams and ask their name
+        try {
 			/*
 			 * Create input and output streams for this client.
 			 */
-			inStream = new DataInputStream(clientSocket.getInputStream());
-			outStream = new PrintStream(clientSocket.getOutputStream());
-			String name = inStream.readLine();
+            inStream = new DataInputStream(clientSocket.getInputStream());
+            outStream = new PrintStream(clientSocket.getOutputStream());
+            String name = inStream.readLine();
 
-			//Only run if it is not a socket that wants only mapdata
-			if(name != "mapDataSocket")
-			{
-			
 			/* Welcome the new the client. */
-                outStream.println("{<" + name + "> X:" + character.getX() + " Y:" + character.getY() + "}");
-                TCPServer.getInstance().numOfConnectedClients++;
+            outStream.println("{<" + name + "> X:" + character.getX() + " Y:" + character.getY() + "}");
+            TCPServer.getInstance().numOfConnectedClients++;
 
-                outStream.flush();
-                //Database.insertIntoHighscoreDatabase(this.getID(), clientName, 10);
-                //Database.printHighscoreDatabase();
-                // Send player update info to all the other clients
-                synchronized (this) {
-                    for (int i = 0; i < maxClientsCount; i++) {
-                        if (threads[i] != null && threads[i] == this) {
-                            System.out.println("Player " + name + " Joined.");
-                            clientName = name;
-                            break;
-                        }
+            outStream.flush();
+            //Database.insertIntoHighscoreDatabase(this.getID(), clientName, 10);
+            //Database.printHighscoreDatabase();
+            // Send player update info to all the other clients
+            synchronized (this) {
+                for (int i = 0; i < maxClientsCount; i++) {
+                    if (threads[i] != null && threads[i] == this) {
+                        System.out.println("Player " + name + " Joined.");
+                        clientName = name;
+                        break;
                     }
-                    for (int i = 0; i < maxClientsCount; i++) {
-                        if (threads[i] != null && threads[i] != this) {
-                            threads[i].outStream.println("{<" + name + "> X:" + character.getX() + " Y:" + character.getY() + "}");
-                        }
+                }
+                for (int i = 0; i < maxClientsCount; i++) {
+                    if (threads[i] != null && threads[i] != this) {
+                        threads[i].outStream.println("{<" + name + "> X:" + character.getX() + " Y:" + character.getY() + "}");
                     }
                 }
             }
 
 
 
+
 			/* Each Thread will Process their command */
-			while (true) 
-			{
-				String line = inStream.readLine().trim();
+            while (true) {
+                String line = inStream.readLine().trim();
 
 
-				//String used for getting values from json
+                //String used for getting values from json
                 String retrunString = "null";
 
-				ir.ProcessPlayerMovement(line, character);
+                ir.ProcessPlayerMovement(line, character);
 
-				// Proccess the json
-                try{
-                    ir.ProcessJson(line,retrunString, character, map);
-                }
-                catch(JSONException e)
-                {
+                // Proccess the json
+                try {
+                    ir.ProcessJson(line, retrunString, character, map);
+                } catch (JSONException e) {
                     System.out.println("JSON ERRROR: " + e);
                 }
 
                 // Send celldata if cellData is requested
-                if(retrunString != "null")
-                {
+                if (retrunString != "null") {
                     System.out.println("Return String = " + retrunString);
-                    if(retrunString!= "null")
+                    if (retrunString != "null")
                         outStream.println(retrunString);
                 }
 
 
                 outStream.flush();
 
-				// Sends number of players ins session
-				if(line.equals("NUMBER_OF_PLAYERS_REQUEST"))
-				{
-					int numberOfCurrentPlayers = 0;
-					for (int j = 0; j < maxClientsCount; j++) 
-					{
-						if (threads[j] != null && threads[j].clientName != null) 
-						{
-							numberOfCurrentPlayers++;
-						}
-					}
-					//Print player number
-					outStream.println("[#:" + 0 + numberOfCurrentPlayers +"].");
-					outStream.flush();
-				}
-
-
-				//Process actions
-				if(line.equals("PLACE_BED") || line.equals("PLACE_BOX"))
-				{
-					//map.setCell(character.getX() / map.getCellSize(), character.getY() / map.getCellSize(), line);
-				}
-
-
-
-
-				// Send all players positions to all clients
-
-                    // concurrently send strings of player positions to all the clients
-                    for (int i = 0; i < maxClientsCount; i++) {
-                        if (threads[i] != null && threads[i].clientName != null)
-                        {
-								outStream.println("{<" + threads[i].clientName + "> X:" + threads[i].character.getX() + ". Y:" + threads[i].character.getY() + ".}\n");
+                // Sends number of players ins session
+                if (line.equals("NUMBER_OF_PLAYERS_REQUEST")) {
+                    int numberOfCurrentPlayers = 0;
+                    for (int j = 0; j < maxClientsCount; j++) {
+                        if (threads[j] != null && threads[j].clientName != null) {
+                            numberOfCurrentPlayers++;
                         }
                     }
-
-				
-
-				// Disconnect the client if they send the quit message
-				if(line.equals("QUIT"))
-				{
-					synchronized (this) 
-					{
-						for (int i = 0; i < maxClientsCount; i++) 
-						{
-							if (threads[i] != null && threads[i] != this && threads[i].clientName != null) 
-							{
-								threads[i].outStream.println("The player " + name + " is leaving");
-							}
-						}
-					}
-					System.out.println("Player " + name + " Leaving");
+                    //Print player number
+                    outStream.println("[#:" + 0 + numberOfCurrentPlayers + "].");
+                    outStream.flush();
+                }
 
 
-					synchronized (this) 
-					{
-						for (int i = 0; i < maxClientsCount; i++) {
-							if (threads[i] == this) {
-								threads[i] = null;
-							}
-						}
-					}
+                //Process actions
+                if (line.equals("PLACE_BED") || line.equals("PLACE_BOX")) {
+                    //map.setCell(character.getX() / map.getCellSize(), character.getY() / map.getCellSize(), line);
+                }
+
+
+                // Send all players positions to all clients
+                // concurrently send strings of player positions to all the clients
+                for (int i = 0; i < maxClientsCount; i++) {
+                    if (threads[i] != null && threads[i].clientName != null) {
+                        //outStream.println("{<" + threads[i].clientName + "> X:" + threads[i].character.getX() + ". Y:" + threads[i].character.getY() + ".}\n");
+                    }
+                }
+
+
+                // Disconnect the client if they send the quit message
+                if (line.equals("QUIT")) {
+                    synchronized (this) {
+                        for (int i = 0; i < maxClientsCount; i++) {
+                            if (threads[i] != null && threads[i] != this && threads[i].clientName != null) {
+                                threads[i].outStream.println("The player " + name + " is leaving");
+                            }
+                        }
+                    }
+                    System.out.println("Player " + name + " Leaving");
+
+
+                    synchronized (this) {
+                        for (int i = 0; i < maxClientsCount; i++) {
+                            if (threads[i] == this) {
+                                threads[i] = null;
+                            }
+                        }
+                    }
 					/*
 					 * Close the output stream, close the input stream, close the
 					 * socket.
 					 */
-					
-					inStream.close();
-					outStream.close();
-					clientSocket.close();
-				}
-			}
-		} catch (IOException e) 
-		{
-			System.out.println("Error in client thread: " + e);
-		}
-	}
 
-	// Function for getting the client ID for each client thread
-	public long GetClientThread() {
+                    inStream.close();
+                    outStream.close();
+                    clientSocket.close();
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error in client thread: " + e);
+        }
+    }
 
-		return Thread.currentThread().getId();
-	}
+    // Function for getting the client ID for each client thread
+    public long GetClientThread() {
 
-	public Character GetCharacter() {
-		return character;
-	}
-	//Our Code Ends////////////////////////////////////////////////////////
+        return Thread.currentThread().getId();
+    }
+
+    public Character GetCharacter() {
+        return character;
+    }
+    //Our Code Ends////////////////////////////////////////////////////////
 
 }
